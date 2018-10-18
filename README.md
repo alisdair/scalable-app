7982a5be844ece9ed2fb14c149cfa9553368a0a6
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2018-10-18 09:25:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# scalable-app
+
+It goes to 11.
---
+++ aws.tf
@@ -0,0 +1,31 @@
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  count         = "${var.web_count}"
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
